48aca68dc5daca5cf190df1c9f0b1f617ef20703
Bibek Shrestha <sbibek@hotmail.com>
2021-09-18 23:02:52 -0700

    done

--- bc.py
+++ bc.py
@@ -3,21 +3,30 @@ import datetime as date
 import random
 import copy
 
+# Simple MerkleTree Implementation 
+# This class takes in leaves which are transactions of the block lets say T  
+# It uses sha256 to hash all the transactions in T and then incrementally
+# combine two hashes to form single parent hash until we get last one hash which is merkle root
+# Array of hashes are returned where first hash is the root
 class MerkleTree:
+
+  # Returns merkle tree for given transactions
   def getTree(self, transactions):
+    # if there is odd number of transactions, we add one dummy one to make it even
     if len(transactions) % 2 != 0:
       transactions.append({})
-
     tree = []
-    
     leaves = []
+    # get hash of the leaves
     for t in transactions:
       leaves.append(self.__getHash(t))  
     tree.append(leaves)
 
+    # create incremental parent hashes until root is reached
     self.__genTree(transactions, tree)
     return list(reversed(tree))
 
+  # Recursive function to calculate parent root until root
   def __genTree(self, transactions, result):
     tns = []
     for i in range(0, len(transactions), 2):
@@ -27,12 +36,17 @@ class MerkleTree:
     if len(tns) > 1:
       self.__genTree(tns, result)
 
-
+  # use sha256 for merkle tree
   def __getHash(self, transaction):
     sha = hasher.sha256()
     sha.update(str(transaction).encode('utf-8'))
     return sha.hexdigest()
 
+
+# Block of the Blockchain 
+# Holds the transactions, timestamp, merkletree, nonce, 
+# previous and current hashes
+#
 class Block:
   def __init__(self, index, timestamp, transactions, previous_hash, nonce):
     self.index = index
@@ -43,11 +57,13 @@ class Block:
     self.previous_hash = previous_hash
     self.hash = self.hashBlock()
 
+  # Converts the block parameters to string and then calculates the hash
+  # using sha256
   def hashBlock(self):
     sha = hasher.sha256()
     sha.update(
       str(self.timestamp).encode('utf-8') +
-      str(self.transactions).encode('utf-8') + # to be replaced by merkle root 
+      str(self.transactions).encode('utf-8') + 
       str(self.merkletree[0][0] if len(self.merkletree) > 0 else "").encode('utf-8') +
       str(self.nonce).encode('utf-8') +
       str(self.previous_hash).encode('utf-8')
@@ -55,12 +71,18 @@ class Block:
     return sha.hexdigest()
   
 
+# Blockchain holds all the mined blocks and also the current block
+# current block holds the transactions and is added to the blocks
+# after it is mined
 class Blockchain:
   def __init__(self):
     self.blocks = []
     self.currentblock = Block(0, date.datetime.now(),[], "0", 0) 
     self.merklefn = MerkleTree()
 
+  # adding transaction would first make sure that the transaction is valid
+  # merkle tree is also calculated and added to the block if its valid
+  # finally transaction is added to current block if it is validated
   def addTransactions(self, transaction):
     verdict = self.validateTransaction(transaction)
     if verdict:
@@ -68,6 +90,8 @@ class Blockchain:
       self.currentblock.merkletree = self.merklefn.getTree(copy.deepcopy(self.currentblock.transactions))
     return verdict
 
+  # Utility function to validate transaction
+  # A transaction is valid is the user transferring the amount has sufficient funds to do so
   def validateTransaction(self, transaction):
     fromUser = transaction['from']
     # now we will check if this user has enough fund to do this transaction
@@ -86,9 +110,11 @@ class Blockchain:
     # being spend is something the user has
     return availableFunds >= transaction['amount']
 
+  # Utility function to get last block
   def lastBlock(self):
     return self.blocks[-1]
 
+  # Adds the current block to the blockchain
   def commitBlock(self, block):
     blk = copy.deepcopy(block) 
     blk.hash = block.hash
@@ -96,25 +122,34 @@ class Blockchain:
     self.currentblock = Block(0, date.datetime.now(),[], "0", 0) 
 
 
+# Miner is depection of individual miners 
+# Each miner is given a name and the amount of cpus it has 
+# which shows how much mining power it has
 class Miner:
   def __init__(self, name, cpu=1):
     self.name = name
     self.cpu = cpu
     self.blockchain = Blockchain()
-  
+
+  # log the current state of blockchain in the current miner 
   def log(self):
     print("\n******** {} ********".format(self.name))
     i = 0
     for block in self.blockchain.blocks:
+      print(" <{}> timestamp:{}".format(i, block.timestamp))
       print(" <{}> nonce: {} transactions: {}".format(i, block.nonce, str(block.transactions)))
       print(" <{}> root of merkle tree : {}".format(i, str(block.merkletree[0][0]) if len(block.merkletree) > 0 else ""))
       print(" <{}> prev_hash: {} hash: {}".format(i, block.previous_hash, block.hash))
       print()
       i += 1
 
+  # Add transaction to the blockchain of this miner
   def addTransaction(self, transaction):
     return self.blockchain.addTransactions(copy.deepcopy(transaction))
-  
+
+  # Mine the current block
+  # Since this is just simulation, so we instead of running each miner individually, we will run the one by one
+  # providing previous failed nonce +1 for trying to mine until hashDifficulty is satisfied
   def tryNextMining(self, index, currentNonce, hashDifficulty):
     # lets update the previous hash and index
     self.blockchain.currentblock.index = index
@@ -128,18 +163,19 @@ class Miner:
       block.hash = m
     return verdict, block
 
-  def updateCurrentBlock(self, nonce, hash):
-    self.blockchain.currentblock
-  
+  # Validate block to check if the nonce is correct and the previous hash is correct too
   def validateBlock(self, block, hashDifficult):
     bhash = int(block.hashBlock(),16) 
     return bhash < hashDifficult and self.blockchain.lastBlock().hash == block.previous_hash
 
+  # commit the current block to the blockchain of this miner
   def commitBlock(self, block):
     self.blockchain.commitBlock(block)
 
 
-
+# Simulate the blockchain with multiple miners
+# We assume that there is pre-existing transactions which be mined for all blocks
+# before simulation starts
 class Simulation:
   def __init__(self, miners, predefinedTransactions=[], simulatedTransactions=[]):
     # set difficulty for the proof of work
@@ -154,6 +190,7 @@ class Simulation:
     self.predefinedTransactions = predefinedTransactions
     self.sumulatedTransactions = simulatedTransactions
 
+  # miner for pre-exsting transactions, block
   def mineBlock(self, block):
     m = block.hashBlock()
     while int(m, 16) >= self.difficulty_hash:
@@ -162,6 +199,7 @@ class Simulation:
     block.hash = m
     return m 
 
+  # broadcasts the mined block to all the miners
   def broadcastMinedBlock(self, block):
     for miner in self.uniqueMiners:
       if miner.validateBlock(block, self.difficulty_hash):
@@ -175,7 +213,10 @@ class Simulation:
     for miner in self.uniqueMiners:
       miner.log()
 
-
+  # start the simulation
+  # first create the genesis block
+  # then mine all the pre-existing blocks
+  # then simulate all the transactions
   def simulate(self):
     index = 0
     genesisBlock = Block(index, date.datetime.now(),{"Genesis": "Block"}, "0", 0)
@@ -230,7 +271,8 @@ class Simulation:
           break
 
 
-miner1 = Miner("Debra", cpu=1)
+# Miners for this simulation
+miner1 = Miner("Arif", cpu=1)
 miner2 = Miner("Akin", cpu=2)
 miner3 = Miner("Nagmat", cpu=3)
 
@@ -242,6 +284,7 @@ predefinedTransactions = [
   {'from': 'grahm', 'to': 'charlie', 'amount': 100 }
 ]
 
+# These transactions will be simulated
 simulatedTransactions = [
   {'from': 'alice', 'to': 'bob', 'amount': 10},
   {'from': 'alice', 'to': 'charlie', 'amount': 50},
@@ -251,6 +294,7 @@ simulatedTransactions = [
   {'from': 'charlie', 'to': 'alice', 'amount': 30},
 ]
 
+# start the simulation
 simulation = Simulation(miners=[miner1, miner2, miner3], predefinedTransactions=predefinedTransactions, simulatedTransactions = simulatedTransactions)
 simulation.simulate()
 simulation.log()
